36d9659158272998ba4cd2db09cfc478a8c0f52e
Ca-rlos <carlos.eft.social@gmail.com>
2021-03-16 16:53:30 +0900

    switching got to axios, fixing some of the GCP stuff

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-const got = require('got');
+const axios = require('axios');
 const {BigQuery} = require('@google-cloud/bigquery');
 const bigquery = new BigQuery();
 
 exports.recentTweet = async function recentTweet(req, res) {
     try {
-        const options = {headers: {"Authorization": "Bearer " + process.env.BEARER_TOKEN}}
-	    const response = await got('https://api.twitter.com/2/users/' + req.query.user_id + '/tweets', options);
+        const options = {headers: {'Authorization': 'Bearer ' + process.env.BEARER_TOKEN}};
+        const request = await axios.get('https://api.twitter.com/2/users/' + req.query.user_id + '/tweets', options);
         const datasetId = 'twitter';
         const tableId = 'recent_tweets';
-        for await (tweet of response.body.data) {
-            const row = {
+        request.data.data.forEach(tweet => {bigquery
+            .dataset(datasetId)
+            .table(tableId)
+             .insert({
                 userId: req.query.user_id,
                 tweetId: tweet.id,
                 tweetText: tweet.text
-            };
-            await bigquery
-                .dataset(datasetId)
-                .table(tableId)
-                .insert(row);
-        };
-        await res.send('success!')
+            })
+        });
+        res.send('success!');
     } catch (error) {
 	    console.log(error);
         res.send('error!');
